3b84b6b85476c6217cba3eb016ea15b6132f4b7a
geyinomercan <93309479+geyinomercan@users.noreply.github.com>
2024-11-29 22:35:46 +0300

    Gunler eklendi.

--- js/clock.js
+++ js/clock.js
@@ -7,7 +7,25 @@ function showTime() {
     let hour = TODAY.getHours()
     let minute = TODAY.getMinutes()
     let second = TODAY.getSeconds()
-    let time = `${hour} : ${minute} : ${second}`
+    let day = TODAY.getDay()
+    
+    if(day == 0){
+        day = "Pazar"
+    }else if(day == 1){
+        day = "Pazartesi"
+    }else if(day == 2){
+        day = "Sali"
+    }else if(day == 3){
+        day = "Carsamba"
+    }else if(day == 4){
+        day = "Persembe"
+    }else if(day == 5){
+        day = "Cuma"
+    }else if(day == 6){
+        day = "Cumartesi"
+    }
+
+    let time = `Bugun ${day} ve saat:${hour}.${minute}.${second}`
     myClock.innerHTML = time
     setTimeout(showTime, 1000)
 }
